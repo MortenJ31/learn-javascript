8cc39c2e798974e0236e5c19110553d69923a962
MortenJ31 <mortenjeppesen91@gmail.com>
2024-05-18 15:41:32 +0200

    Upload to github

---
+++ 1. Build a Passenger Counter App/01. Welcome to the course/tempCodeRunnerFile.js
@@ -0,0 +1,20 @@
+function increment() {
+    console.log("The button was clicked")
+}
+
+function Number() {
+    console.log(42)
+}
+
+Number()
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function sumLaps() {
+    let totalLaps = lap1 + lap2 + lap3
+    console.log(totalLaps)
+}
+
+sumLaps()
